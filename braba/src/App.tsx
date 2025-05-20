fd9c084dc9be6c3e0bed38c040b29a47f35e9dd3
vdzxs015 <vi.daniel16@gmail.com>
2025-05-20 14:49:16 -0300

    feat: implement Sheet and Sidebar components with responsive design
    - Added Sheet component for modal-like functionality using Radix UI.
    - Created Sidebar component with collapsible and mobile-friendly features.
    - Introduced Skeleton component for loading states.
    - Implemented Tooltip component for enhanced user interactions.
    - Added useIsMobile hook to manage mobile state.
    - Established utility functions for class name management.
    - Configured Tailwind CSS for styling and responsive design.
    - Set up TypeScript configuration for improved type safety.
    - Integrated Vite for development and build processes.

---
+++ braba/src/App.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+
+
+
+
+function App() {
+  return (
+    <div >
+      <Button>Click me</Button>
+      
+<Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetDescription>
+       
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+
+
+      <AlertDialog>
+  <AlertDialogTrigger>Open</AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+
+    </div>
+  )
+}
+
+export default App
